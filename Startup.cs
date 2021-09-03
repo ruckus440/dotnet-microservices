c731126486dc14e6e2411253b3279f4ed80dc122
ruckus440 <mwruckert@gmail.com>
2021-09-03 16:51:21 -0500

    Part 2 done

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace PlatformService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
             services.AddScoped<IPlatformRepo, PlatformRepo>();
             
